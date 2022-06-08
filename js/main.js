9b1dd92b607b35237a156c9402fe5b799d0c4703
Natalya Sosidka <natapolyakova13@gmail.com>
2022-06-08 21:13:53 +0300

    добавляет переменную

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ getRandomIntInclusive(2, 9);
 
 function getRandomArbitrary(min, max, digitAmount = 0) {
   if (min >= 0 && max >= 0 && max > min) {
-    digitDegree = 10 ** digitAmount;
+    const digitDegree = 10 ** digitAmount;
     return ~~ ((Math.random() * (max - min + 1) + min) * digitDegree) / digitDegree;
   } else {
     console.log('Введены некорректные данные');
