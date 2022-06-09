66e2fe6f90a12eab7087d4ea8bfb8fe595de7b27
Natalya Sosidka <natapolyakova13@gmail.com>
2022-06-09 21:36:39 +0300

    вносит исправления в соответствии с комментариями наставика

--- js/main.js
+++ js/main.js
@@ -2,20 +2,22 @@ function getRandomIntInclusive(min, max) {
   if (min >= 0 && max >= 0 && max > min) {
     min = Math.ceil(min);
     max = Math.floor(max);
+
     return Math.floor(Math.random() * (max - min + 1)) + min;
   } else {
-    return ('Введены некорректные данные');
+    return 'Введены некорректные данные';
   }
 }
 
 getRandomIntInclusive(2, 9);
 
-function getRandomArbitrary(min, max, digitAmount = 0) {
+function getRandomArbitrary(min, max, digitNumber = 0) {
   if (min >= 0 && max >= 0 && max > min) {
-    const digitDegree = 10 ** digitAmount;
-    return ~~ ((Math.random() * (max - min + 1) + min) * digitDegree) / digitDegree;
+    const randomNumber = (Math.random() * (max - min + 1) + min);
+
+    return randomNumber.toFixed(digitNumber);
   } else {
-    return ('Введены некорректные данные');
+    return 'Введены некорректные данные';
   }
 }
 
