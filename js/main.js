afa4313e2ac61adda61d502e6503c5a521df7052
Natalya Sosidka <natapolyakova13@gmail.com>
2022-06-17 22:53:30 +0300

    Больше деталей2

--- js/main.js
+++ js/main.js
@@ -28,8 +28,8 @@ function getGuestsValue () {
 
 const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const CHECKTIME = ['12:00', '13:00', '14:00'];
-const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+// const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+// const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 
 
 const createAds = function () {
@@ -61,3 +61,5 @@ const createAds = function () {
     }
   };
 };
+
+createAds();
