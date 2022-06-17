9d6851af9e61de8ff45f9ca7e8e3e2372635ee91
Natalya Sosidka <natapolyakova13@gmail.com>
2022-06-17 22:51:07 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,24 +1,63 @@
-function getRandomIntInclusive(min, max) {
-  if (min >= 0 && max >= 0 && max > min) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
 
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  } else {
-    return 'Введены некорректные данные';
-  }
+  return Math.floor(result);
 }
 
-getRandomIntInclusive(2, 9);
+function getRandomPositiveFloat (a, b, digits = 5) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
 
-function getRandomArbitrary(min, max, digitNumber = 0) {
-  if (min >= 0 && max >= 0 && max > min) {
-    const randomNumber = (Math.random() * (max - min + 1) + min);
-
-    return randomNumber.toFixed(digitNumber);
-  } else {
-    return 'Введены некорректные данные';
-  }
+  return +result.toFixed(digits);
 }
 
-getRandomArbitrary(0, 5, 4);
+function getRoomsValue () {
+  const maxRoomsValue = 6;
+
+  return Math.floor(Math.random() * Math.abs(maxRoomsValue));
+}
+
+function getGuestsValue () {
+  const maxRoomsValue = 6;
+
+  return Math.floor(Math.random() * Math.abs(maxRoomsValue));
+}
+
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECKTIME = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
+
+const createAds = function () {
+  const randomTypeIndex = getRandomPositiveInteger(0, TYPES.length - 1);
+  const randomCheckIndex = getRandomPositiveInteger(0, CHECKTIME.length - 1);
+  const randomLat = getRandomPositiveFloat(35.65, 35.7);
+  const randomLng = getRandomPositiveFloat(139.7, 139.8);
+
+  return {
+    author: {
+      avatar: ''
+    },
+    offer: {
+      title: '',
+      adress: `${randomLat  }, ${  randomLng}`,
+      price: '',
+      type: TYPES[randomTypeIndex],
+      rooms: getRoomsValue(),
+      guests: getGuestsValue(),
+      checkin: CHECKTIME[randomCheckIndex],
+      checkout: CHECKTIME[randomCheckIndex],
+      features: [],
+      description: '',
+      photos: []
+    },
+    location: {
+      lat: randomLat,
+      lng: randomLng
+    }
+  };
+};
